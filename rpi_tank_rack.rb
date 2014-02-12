0a7ce0a2cb223f687c6f1d066182bd47b01033c7
Artem Sheremet <dot.doom@gmail.com>
2014-02-13 02:22:06 +0300

    UI and backend improvements for Tank mode
    * smaller arrows
    * Classic and Tank view with auto switching
    * signal repeating and throttling
    * protect against burning the engine (both directions)
    
    TODO: figure out reverse directions for tracks

--- rpi_tank_rack.rb
+++ rpi_tank_rack.rb
@@ -80,14 +80,85 @@ class WebApplication
 end
 
 class SocketControlApplication < Rack::WebSocket::Application
+	class PowerState
+		class Directionable
+			attr_reader :direction
+
+			# Protect against double directioning
+			def direction=(new_dir)
+				if @direction && new_dir
+					# Lockout
+					@direction = false
+				elsif @direction != false
+					@direction = new_dir
+				end
+				new_dir
+			end
+
+			def reset
+				@direction = nil
+			end
+
+			def to_pin
+				@pins[@direction]
+			end
+		end
+
+		class Track < Directionable
+			def initialize(forward_pin, reverse_pin)
+				@pins = {
+					forward: forward_pin,
+					reverse: reverse_pin,
+				}
+			end
+		end
+
+		attr_reader :track_left, :track_right
+
+		class Tower < Directionable
+			def initialize(left_pin, right_pin)
+				@pins = {
+					left: left_pin,
+					right: right_pin,
+				}
+			end
+		end
+
+		attr_reader :tower
+
+		def initialize
+			@track_left = Track.new(26, nil) # TODO: 24?
+			@track_right = Track.new(23, nil) # TODO: 22?
+			@tower = Tower.new(21, 19)
+		end
+
+		def reset
+			[@track_left, @track_right, @tower].each(&:reset)
+		end
+
+		def to_pin
+			[@track_left, @track_right, @tower].map(&:to_pin).compact.join(' ')
+		end
+
+		def to_s
+			[@track_left, @track_right, @tower].zip(%w(LEFT RIGHT TOWER)).map { |object, name|
+				"#{name}: #{object.direction}" if object.direction
+			}.compact.join(', ')
+		end
+	end
+
 	CONTROLS = {
-		'left'        => '23',
-		'right'       => '26',
-		'forward'     => '26 23',
-		'backward'    => '24 22',
-		'tower_left'  => '21',
-		'tower_right' => '19',
-		'stop'        => '',
+		'engine_left'        => -> ps { ps.track_right.direction = :forward },
+		'engine_right'       => -> ps { ps.track_left.direction = :forward },
+		'engine_forward'     => -> ps { ps.track_left.direction = ps.track_right.direction = :forward },
+		'engine_reverse'     => -> ps { ps.track_left.direction = ps.track_right.direction = :reverse },
+		'tower_left'         => -> ps { ps.tower.direction = :left },
+		'tower_right'        => -> ps { ps.tower.direction = :right },
+		'trackleft_forward'  => -> ps { ps.track_left.direction = :forward },
+		'trackright_forward' => -> ps { ps.track_right.direction = :reverse },
+		'trackleft_reverse'  => -> ps { ps.track_left.direction = :forward },
+		'trackright_reverse' => -> ps { ps.track_right.direction = :reverse },
+		'stop'               => -> ps {},
 	}
 
 	def on_open(env)
@@ -100,9 +171,10 @@ class SocketControlApplication < Rack::WebSocket::Application
 	end
 
 	def on_message(env, msg)
-		translated_message = CONTROLS[msg]
-		puts "WebSocket: Message #{msg.inspect} => #{translated_message.inspect}"
-		connection.puts "set_output #{translated_message}"
+		power_state.reset
+		msg.split.each { |control| CONTROLS[control].call(power_state) } rescue false
+		puts "WebSocket: message #{msg.inspect} => #{power_state.to_s.inspect}"
+		connection.puts "set_output #{power_state.to_pin}"
 	rescue
 		puts "WebSocket: #$!"
 	end
@@ -113,6 +185,10 @@ class SocketControlApplication < Rack::WebSocket::Application
 
 	def connection
 		@connection ||= TCPSocket.new 'localhost', 11700
+	end
+
+	def power_state
+		@power_state ||= PowerState.new
 	end
 end
 
